74dbc172bb1e04ad8659c645a03704256c9b50d3
Athanasius <github@miggy.org>
2021-05-01 20:24:25 +0100

    setup for py2exe

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+import py2exe
+
+setup(windows=[{"script": "sample_systray.py"}],
+      options={"py2exe": {
+               "verbose": 4}
+              },
+     )
