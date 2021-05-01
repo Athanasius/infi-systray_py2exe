aab2caa3b0cd47bd06b0c14cb78be56415d4e1ef
Athanasius <github@miggy.org>
2021-05-01 20:28:19 +0100

    Attempt to inclulde pkg_resources in build

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from distutils.core import setup
 import py2exe
 
-setup(windows=[{"script": "sample_systray.py"}],
-      options={"py2exe": {
-               "verbose": 4}
-              },
-     )
+setup(
+    windows=[{'script': 'sample_systray.py'}],
+    options={
+        'py2exe': {
+            'verbose': 4,
+            'includes': [
+                'pkg_resources',
+            ],
+        }
+    },
+)
